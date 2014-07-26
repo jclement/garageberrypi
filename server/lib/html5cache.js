0012e7a2240fca5bcbb2d0a7c922ce2906ca3bc4
Jeff Clement <jeffrey.clement@gmail.com>
2014-07-26 16:08:24 +0000

    more fixes to offline cache

--- server/lib/html5cache.js
+++ server/lib/html5cache.js
@@ -37,27 +37,42 @@ var fullpath = function(f) {
 
 // Hook of FS watchers so that changes to DEV files update the serial number
 _.each(WATCHED, function(file) {
+  console.log('Watching ' + fullpath(file));
   fs.watch(fullpath(file), function() {
+    console.log('Watched file ' + fullpath(file) + ' changed');
     serial = new Date().valueOf();
   });
 });
 
 // Handle a request for cache manifest, turns off all caching of response
-var requestHandler = function(req, res) {
-  var tmp = 'CACHE MANIFEST\n# rev ' + serial + '\n\n';
-  tmp += 'CACHE:\n';
-  _.each(CACHED, function(f) {
-    tmp += f + '\n';
-  });
-  tmp += '\nNETWORK:\n';
-  _.each(NETWORK, function(f) {
-    tmp += f + '\n';
-  });
-  res.set('Content-Type','text/application-manifest');
-  res.set('Cache-Control','no-store, no-cache, must-revalidate, max-age=0, post-check=0, pre-check=0');
-  res.set('Pragma', 'no-cache');
-  res.set('Expires', 'Tue, 03 Jul 2001 06:00:00 GMT');
-  res.send(tmp);
-};
+var requestHandler = (function() {
+  var lastSerial = null;
+  var genDate = null;
+  var body = null;
+  return function(req, res) {
+    if (serial !== lastSerial) {
+      lastSerial = serial;
+      genDate 
+      var tmp = 'CACHE MANIFEST\n# rev ' + serial + '\n\n';
+      tmp += 'CACHE:\n';
+      _.each(CACHED, function(f) {
+        tmp += f + '\n';
+      });
+      tmp += '\nNETWORK:\n';
+      _.each(NETWORK, function(f) {
+        tmp += f + '\n';
+      });
+      body = tmp;
+      genDate = new Date();
+    }
+    res.set('Content-Type','text/application-manifest');
+    res.set('Cache-Control','no-store, no-cache, must-revalidate, max-age=0');
+    res.set('Pragma', 'no-cache');
+    res.set('Expires', 'Tue, 03 Jul 2001 06:00:00 GMT');
+    res.set('Last-Modified', genDate);
+    res.set('ETag', null);
+    res.send(body);
+  };
+})();
 
 module.exports.requestHandler = requestHandler;
